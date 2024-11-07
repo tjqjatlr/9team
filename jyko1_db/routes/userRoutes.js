d434505169a69e0e0faa4aaa300c2f06c0633d4e
jyko1 <167264782+jyko1@users.noreply.github.com>
2024-11-07 17:25:46 +0900

    Add files via upload

---
+++ jyko1_db/routes/userRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const User = require('../app/User');
+const router = express.Router();
+
+// 사용자 추가 라우트
+router.post('/users', async (req, res) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// 사용자 조회 라우트 예제
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+module.exports = router;
